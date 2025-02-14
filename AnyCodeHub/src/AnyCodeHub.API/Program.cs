c475ff42f9de29e15a98a72b414f5e975d9bb352
aquariusdat <tondat.dev@gmail.com>
2025-02-14 15:03:07 +0700

    feat(api): add swagger configuration

--- AnyCodeHub/src/AnyCodeHub.API/Program.cs
+++ AnyCodeHub/src/AnyCodeHub.API/Program.cs
@@ -1,7 +1,9 @@
+using AnyCodeHub.API.DependencyInjection.Extensions;
 using AnyCodeHub.API.Middlewares;
 using AnyCodeHub.Application.DependencyInjections.Extensions;
 using AnyCodeHub.Persistence.DependencyInjections.Extensions;
 using AnyCodeHub.Persistence.DependencyInjections.Options;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +16,7 @@ Log.Logger = new LoggerConfiguration().ReadFrom
                 .CreateLogger();
 builder.Logging.ClearProviders().AddSerilog();
 builder.Host.UseSerilog();
-
+    
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 
 #region Application
@@ -26,13 +28,39 @@ builder.Services.AddMediatRApplication();
 builder.Services.AddAutoMapperApplication();
 #endregion Application
 
+// Add Swagger configrations
+builder.Services
+        .AddSwaggerGenNewtonsoftSupport()
+        .AddFluentValidationRulesToSwagger()
+        .AddEndpointsApiExplorer()
+        .AddSwagger();
+
+builder.Services
+    .AddApiVersioning(options => options.ReportApiVersions = true)
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
 builder.Services.AddPostgreSql();
 builder.Services.AddInterceptors();
 builder.Services.ConfigurePostgreSqlRetryOptions(builder.Configuration.GetSection(nameof(PostgreSqlRetryOptions)));
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
+app.UseAuthentication();
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+    });
+}
 
 app.UseAuthorization();
 
