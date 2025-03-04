99b707af3c1b3eea05b40e7f22a3bfece2067f9f
aquariusdat <tondat.dev@gmail.com>
2025-03-04 16:23:27 +0700

    feat(api): change database connection string

--- AnyCodeHub/src/AnyCodeHub.Persistence/ApplicationDbContext.cs
+++ AnyCodeHub/src/AnyCodeHub.Persistence/ApplicationDbContext.cs
@@ -8,18 +8,20 @@ namespace AnyCodeHub.Persistence
     public sealed class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
     {
 
-        public ApplicationDbContext(DbContextOptions options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder) => builder.ApplyConfigurationsFromAssembly(Persistence.AssemblyReferences.Assembly);
-        public DbSet<AppUser> AppUses { get; set; }
-        public DbSet<Function> Functions { get; set; }
-        public DbSet<ActionInFunction> ActionInFunctions { get; set; }
-        public DbSet<Permission> Permissions { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder) 
+            => builder.ApplyConfigurationsFromAssembly(Persistence.AssemblyReferences.Assembly);
 
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppUser> AppUses { get; set; }
+        //public DbSet<Function> Functions { get; set; }
+        //public DbSet<ActionInFunction> ActionInFunctions { get; set; }
+        //public DbSet<Permission> Permissions { get; set; }
+
+        //public DbSet<Course> Courses { get; set; }
+        //public DbSet<Category> Categories { get; set; }
 
     }
 }
