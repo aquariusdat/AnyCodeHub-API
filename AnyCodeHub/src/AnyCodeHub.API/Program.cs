5da47a6e61052fe3c54abef54e46d493efb05e54
aquariusdat <tondat.dev@gmail.com>
2025-04-04 14:36:41 +0700

    feat(api): Auto generate migration db

--- AnyCodeHub/src/AnyCodeHub.API/Program.cs
+++ AnyCodeHub/src/AnyCodeHub.API/Program.cs
@@ -2,9 +2,11 @@ using AnyCodeHub.API.DependencyInjection.Extensions;
 using AnyCodeHub.API.Middlewares;
 using AnyCodeHub.Application.DependencyInjections.Extensions;
 using AnyCodeHub.Infrastructure.DependencyInjections.Extensions;
+using AnyCodeHub.Persistence;
 using AnyCodeHub.Persistence.DependencyInjections.Extensions;
 using AnyCodeHub.Persistence.DependencyInjections.Options;
 using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,7 +19,7 @@ Log.Logger = new LoggerConfiguration().ReadFrom
                 .CreateLogger();
 builder.Logging.ClearProviders().AddSerilog();
 builder.Host.UseSerilog();
-    
+
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 
 builder.Services.AddPostgreSql();
@@ -81,5 +83,15 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+if (!builder.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        if (context.Database.GetPendingMigrations().Any()) context.Database.Migrate();
+    }
+}
 
 app.Run();
