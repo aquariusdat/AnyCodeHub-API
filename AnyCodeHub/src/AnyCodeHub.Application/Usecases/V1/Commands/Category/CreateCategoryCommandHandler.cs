8ea9f0ff9af48f6214859712d1f8afb8d6e0b22f
aquariusdat <tondat.dev@gmail.com>
2025-05-12 15:18:31 +0700

    test

--- AnyCodeHub/src/AnyCodeHub.Application/Usecases/V1/Commands/Category/CreateCategoryCommandHandler.cs
+++ AnyCodeHub/src/AnyCodeHub.Application/Usecases/V1/Commands/Category/CreateCategoryCommandHandler.cs
@@ -29,6 +29,13 @@ public class CreateCategoryCommandHandler : ICommandHandler<CreateCategoryComman
     {
         try
         {
+            string a = ""
+
+
+                string b = "haha";
+
+            string thisiswtfvariable = "";
+
             // Check if a category with the same name already exists
             var existingCategory = _categoryRepository.FindAll(c => c.Name == request.name && !c.IsDeleted).FirstOrDefault();
             if (existingCategory != null)
